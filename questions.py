4bb5127e8829f76a9d63b88ee86118fbbcfdb0bb
gvidoou <gvidoou@gmail.com>
2015-03-09 15:45:58 +0200

    added main template and bootstrap but not locally

--- questions.py
+++ questions.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return 'HI'
+    return render_template('main.html')
 
 app.run(debug=True)
