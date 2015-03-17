36ab7270157cf290b5d7916ec3196df831997841
gvidoou@gmail.com <gvidoou@gmail.com>
2015-03-17 11:38:16 +0200

    reformed project layout

--- app/views.py
+++ app/views.py
@@ -1,144 +1,11 @@
-from flask import Flask, render_template, url_for, flash, request, redirect, g
-from flask.ext.login import LoginManager, login_user, current_user, logout_user, \
-    login_required
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
-from wtforms import StringField, PasswordField, validators, TextAreaField
-from flask.ext.wtf import Form
-app = Flask(__name__)
-app.config.update(
-    CSRF_ENABLED=True,
-    DEBUG=True,
-    SECRET_KEY='secret key',
-)
+from flask import render_template, url_for, flash, request, redirect, g
+from flask.ext.login import login_user, logout_user, login_required
 
-# All db configurations
-
-# connecting to DB
-engine = create_engine('sqlite:///' + app.root_path + '/questions.db', echo=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-# using declarative method
-Base = declarative_base()
-Base.query = db_session.query_property()
+from app.forms import AddQuestionForm, LoginForm, AnswerForm
+from app.models import Questions, Answers, User, Like, db_session, login_manager
+from app import app
 
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
-
-
-# All models
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    password = Column(String)
-    asked_questions = relationship('Questions')
-    answers = relationship('Answers')
-    like = relationship('Like')
-
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-
-    def __repr__(self):
-        return '<User %s>' % self.name
-
-    def is_active(self):
-        return True
-
-    def is_authenticated(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.id)
-
-
-class Questions(Base):
-    __tablename__ = 'questions'
-    id = Column(Integer, primary_key=True)
-    question = Column(String)
-    details = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    answers = relationship('Answers')
-
-    def __init__(self, details, question, user_id):
-        self.details = details
-        self.question = question
-        self.user_id = user_id
-
-    def get_user(self):
-        return User.query.filter_by(id=self.user_id).first().name
-
-
-class Answers(Base):
-    __tablename__ = 'answers'
-    id = Column(Integer, primary_key=True)
-    answer = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
-    like = relationship('Like')
-    likes = Column(Integer)
-
-    def __init__(self, answer, user_id, question_id):
-        self.answer = answer
-        self.user_id = user_id
-        self.question_id = question_id
-        self.likes = 0
-
-    def get_user(self):
-        return User.query.filter_by(id=self.user_id).first().name
-
-
-class Like(Base):
-    __tablename__ = 'likes'
-    id = Column(Integer, primary_key=True)
-    answer = Column(Integer, ForeignKey('answers.id'), nullable=False)
-    user = Column(Integer, ForeignKey('users.id'), nullable=False)
-
-    def __init__(self, answer, user):
-        self.answer = answer
-        self.user = user
-
-
-# login settings
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def load_user(userid):
-    return User.query.get(int(userid))
-
-
-# Forms
-class AddQuestionForm(Form):
-    question = StringField('Question', validators=[validators.length(3, 30)])
-    details = TextAreaField('Description', validators=[validators.optional()])
-
-
-class LoginForm(Form):
-    name = StringField('Login', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[validators.DataRequired()])
-
-
-class AnswerForm(Form):
-    answer = TextAreaField('Answer', validators=[validators.length(3)])
-
-
-@app.before_request
-def before_request():
-    g.user = current_user
-
-
-# All views
 @app.route('/')
 def main_page():
     questions = Questions.query.all()
@@ -255,9 +122,6 @@ def like(answer_id):
         return redirect(url_for('main_page'))
 
 
-# decelerating models
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-init_db()
-app.run(debug=True)
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
